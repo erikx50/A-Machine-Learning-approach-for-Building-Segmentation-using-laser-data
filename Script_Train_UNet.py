efecde0fdcc740a689de3bab14cab4c951e4d203
erikx50 <erikx50@users.noreply.github.com>
2023-01-31 11:52:22 +0100

    Set sample weights

--- Script_Train_UNet.py
+++ Script_Train_UNet.py
@@ -90,7 +90,7 @@ callback_list = [
 class_weight = {0: 1., 1: 10.}
 
 # Train the model
-results = model.fit(X_train, Y_train, batch_size = 4, epochs = 25, callbacks = callback_list, validation_data = (X_val, Y_val), class_weight=class_weight)
+results = model.fit(X_train, Y_train, batch_size = 4, epochs = 25, callbacks = callback_list, validation_data = (X_val, Y_val), sample_weight=class_weight)
 
 # Save model
 model.save(os.path.normpath('models/recentUNet'))
