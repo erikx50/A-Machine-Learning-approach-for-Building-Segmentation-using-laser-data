6aba2dae0a95bf7affc581f69056c5bd9c8b1f4f
erikx50 <erikx50@users.noreply.github.com>
2023-01-31 11:33:37 +0100

    Set class weights

--- Script_Train_UNet.py
+++ Script_Train_UNet.py
@@ -86,9 +86,11 @@ callback_list = [
     callbacks.EarlyStopping(monitor = 'val_loss', patience = 3)
 ]
 
+# Set class weights
+class_weight = {0: 1., 1: 10.}
 
 # Train the model
-results = model.fit(X_train, Y_train, batch_size = 4, epochs = 25, callbacks = callback_list, validation_data = (X_val, Y_val))
+results = model.fit(X_train, Y_train, batch_size = 4, epochs = 25, callbacks = callback_list, validation_data = (X_val, Y_val), class_weight=class_weight)
 
 # Save model
 model.save(os.path.normpath('models/recentUNet'))
