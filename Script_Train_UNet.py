3d2dd5dad5ae40f2b49c0abe8465a6776a027bb0
erikx50 <erikx50@users.noreply.github.com>
2023-02-01 13:41:36 +0100

    tweaked learning rate

--- Script_Train_UNet.py
+++ Script_Train_UNet.py
@@ -83,14 +83,14 @@ print('Y_validation size: ' + str(len(Y_val)))
 # EarlyStopping -> Stops the training of the model if it doesnt improve after some epochs
 callback_list = [
     callbacks.ModelCheckpoint(os.path.normpath('models/MapAI_UNet_Task1_Checkpoint.h5'), verbose = 1, save_best_only=True),
-    callbacks.EarlyStopping(monitor = 'val_loss', patience = 3)
+    callbacks.EarlyStopping(monitor = 'val_loss', patience = 10)
 ]
 
 # Set class weights
 #class_weight = [0.1, 1]
 
 # Train the model
-results = model.fit(X_train, Y_train, batch_size = 4, epochs = 25, callbacks = callback_list, validation_data = (X_val, Y_val))
+results = model.fit(X_train, Y_train, batch_size = 8, epochs = 50, callbacks = callback_list, validation_data = (X_val, Y_val))
 
 # Save model
 model.save(os.path.normpath('models/recentUNet'))
