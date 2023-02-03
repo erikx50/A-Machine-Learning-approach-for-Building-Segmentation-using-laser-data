8661470fa128eece10184ba1fb3ce63718fffef6
erikx50 <erikx50@users.noreply.github.com>
2023-02-04 00:09:04 +0100

    l

--- code/UNet.py
+++ code/UNet.py
@@ -53,28 +53,32 @@ def unet_large(input_size=(512, 512, 3)):
 
     # Decoder Part
     # Layer 6
-    u6 = layers.Conv2DTranspose(512, (2,2), strides=(2,2), padding = 'same')(c5)
+    #u6 = layers.Conv2DTranspose(512, (2,2), strides=(2,2), padding = 'same')(c5)
+    u6 = layers.Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(c5))
     u6 = layers.concatenate([u6, c4])
     c6 = layers.Conv2D(512, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u6)
     # c6 = layers.Dropout(0.1)(c6)
     c6 = layers.Conv2D(512, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c6)
 
     # Layer 7
-    u7 = layers.Conv2DTranspose(256, (2,2), strides=(2,2), padding = 'same')(c6)
+    #u7 = layers.Conv2DTranspose(256, (2,2), strides=(2,2), padding = 'same')(c6)
+    u7 = layers.Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(c6))
     u7 = layers.concatenate([u7, c3])
     c7 = layers.Conv2D(256, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u7)
     # c7 = layers.Dropout(0.1)(c7)
     c7 = layers.Conv2D(256, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c7)
 
     # Layer 8
-    u8 = layers.Conv2DTranspose(128, (2,2), strides=(2,2), padding = 'same')(c7)
+    #u8 = layers.Conv2DTranspose(128, (2,2), strides=(2,2), padding = 'same')(c7)
+    u8 = layers.Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(c7))
     u8 = layers.concatenate([u8, c2])
     c8 = layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u8)
     # c8 = layers.Dropout(0.1)(c8)
     c8 = layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c8)
 
     # Layer 9
-    u9 = layers.Conv2DTranspose(64, (2,2), strides=(2,2), padding = 'same')(c8)
+    #u9 = layers.Conv2DTranspose(64, (2,2), strides=(2,2), padding = 'same')(c8)
+    u9 = layers.Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(c8))
     u9 = layers.concatenate([u9, c1], axis=3)
     c9 = layers.Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u9)
     # c9 = layers.Dropout(0.1)(c9)
