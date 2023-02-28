717b15bfb29dabb6b312fd5da70e86da86829af7
erikx50 <erikx50@users.noreply.github.com>
2023-02-28 10:20:26 +0100

    increased batch size

--- code/1b_Train_UNet.py
+++ code/1b_Train_UNet.py
@@ -236,7 +236,7 @@ else:
     ]
 
     # Train the model
-    results = model.fit(X_train, Y_train, batch_size=2, epochs=100, callbacks=callback_list, validation_data =(X_val, Y_val), verbose = 2)
+    results = model.fit(X_train, Y_train, batch_size=6, epochs=100, callbacks=callback_list, validation_data =(X_val, Y_val), verbose = 2)
 
     # Save model
 print("Saving model")
